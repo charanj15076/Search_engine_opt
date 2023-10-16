6867c184aeb001315158be153e4404893d590809
charanj15076 <charanjalukuru@csu.fullerton.edu>
2023-10-16 16:57:25 -0700

    added stop_words and punctuation filtering

---
+++ stop_words_func.py
@@ -0,0 +1,30 @@
+def stop_words_punc_filter(tokens, stop_words):
+    filtered_words = []
+    for w in tokens:
+        if w.lower() not in stop_words:
+            filtered_words.append(w)
+    return filtered_words
+
+#example run
+
+stop_words = []
+
+with open('stop_words.txt', "r") as f:
+    stop_words = f.read().split()
+
+word_tokens = ['This',
+ 'is',
+ 'a',
+ 'sample',
+ 'sentence',
+ ',',
+ 'showing',
+ 'off',
+ 'the',
+ 'stop',
+ 'words',
+ 'filtration',
+ '.']
+
+filtered_words = stop_words_punc_filter(word_tokens,stop_words)
+print("filtered_words",filtered_words)
